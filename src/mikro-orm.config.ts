20c438771fbf008a9245e69e3a825d27be805e48
Tanay Raj <tanay.raj76@gmail.com>
2021-07-03 18:38:56 +0530

    user entity and mutation

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -3,13 +3,14 @@ import { Post } from './entities/Post';
 import { __prod__ } from './constants';
 
 import path from 'path';
+import { User } from './entities/User';
 
 export default {
   migrations: {
     path: path.join(__dirname, './migration'),
     pattern: /^[\w-]+\d+\.[tj]s$/,
   },
-  entities: [Post],
+  entities: [Post, User],
   dbName: 'velocity',
   type: 'postgresql',
   password: 'tanayismyname13',
