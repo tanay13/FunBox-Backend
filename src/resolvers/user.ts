077016d6b7d5827b17fba0173d33836fd361fac9
Tanay Raj <tanay.raj76@gmail.com>
2021-07-03 22:45:53 +0530

    login query ad

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -5,7 +5,7 @@ import {
   Field,
   InputType,
   Mutation,
-  Query,
+  ObjectType,
   Resolver,
 } from 'type-graphql';
 import { User } from '../entities/User';
@@ -17,6 +17,23 @@ class UsernamePasswordInput {
   username: string;
   @Field()
   password: string;
+}
+
+@ObjectType()
+class FieldError {
+  @Field()
+  field: string;
+  @Field()
+  message: string;
+}
+
+@ObjectType()
+class UserResponse {
+  @Field(() => [FieldError], { nullable: true })
+  errors?: FieldError[];
+
+  @Field(() => User, { nullable: true })
+  user?: User;
 }
 
 @Resolver()
@@ -35,14 +52,36 @@ export class UserResolver {
     return user;
   }
 
-  @Mutation(() => User)
+  @Mutation(() => UserResponse)
   async login(
     @Arg('options') options: UsernamePasswordInput,
     @Ctx() { em }: MyContext
-  ): Promise<User> {
+  ): Promise<UserResponse> {
     const user = await em.findOne(User, { username: options.username });
+    if (!user) {
+      return {
+        errors: [
+          {
+            field: 'username',
+            message: 'username does not exist',
+          },
+        ],
+      };
+    }
 
-    await em.persistAndFlush(user);
-    return user;
+    const valid = await argon2.verify(user.password, options.password);
+    if (!valid) {
+      return {
+        errors: [
+          {
+            field: 'password',
+            message: 'wrong password',
+          },
+        ],
+      };
+    }
+    return {
+      user,
+    };
   }
 }
